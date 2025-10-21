18f4573613732634f15390ffa5e88f6d0e8afd03
hyfgg <hyfgg20070723@qq.com>
2025-10-21 17:07:02 +0800

    Adding Driver Class

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,54 @@
+import java.util.Scanner;
+
 public class Driver {
 
-    public static void main(String[] args) {
+    Vehicle bus, car, scooter;
+    Scanner input = new Scanner(System.in);
 
+    public static void main(String[] args)
+    {
+        System.out.println();
+        System.out.println("------------------------");
         System.out.println("Vehicle App Version 1.0");
-    }
-}
+        System.out.println("------------------------");
+        System.out.println();
+
+        new Driver();
+
+    } // End of Main
+
+    Driver() {
+        System.out.println("Please Enter Bus data : ");
+        bus = addVehicle();
+        System.out.println(bus);
+        System.out.println("Please Enter Car data : ");
+        car = addVehicle();
+        System.out.println(car);
+        System.out.println("Please Enter Scooter data : ");
+        scooter = addVehicle();
+        System.out.println(scooter);
+        System.out.print("Enter a Number of Passengers to Check : ");
+        int passengers = input.nextInt();
+
+        if(passengers > 5 && passengers <= 10 )
+            System.out.println("Bus is Most Suitable : " + bus);
+        else if(passengers <= 5 && passengers > 2)
+            System.out.println("Car is Most Suitable : " + car);
+        else if(passengers <= 2)
+            System.out.println("Car is Most Suitable : " + car);
+    } // End of Driver Constructor
+
+    public Vehicle addVehicle() {
+        System.out.print("Enter a Make : ");
+        String make = input.nextLine();
+        System.out.print("Enter a Model : ");
+        String model = input.nextLine();
+        System.out.print("Enter Number of Wheels : ");
+        int wheels = input.nextInt();
+        System.out.print("Enter Number of Passengers : ");
+        int passengers = input.nextInt();
+        input.nextLine(); // BUG in Scanner!! - Clearing buffer to read next String
+        return new Vehicle(make,model,wheels,passengers);
+    } // End of addVehicle Method
+} // End of Driver Class
+//////////////////////////////////////////////////////////////////////
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,51 @@
 public class Vehicle {
-}
+    private String make;
+    private String model;
+    private int numWheels;
+    private int numPassengers;
+    public Vehicle() {
+        this.make = "make";
+        this.model = "model";
+        this.numWheels = 0;
+        this.numPassengers = 0;
+    }
+    public Vehicle(String make, String model, int numWheels, int numPassengers) {
+        this.make = make;
+        this.model = model;
+        this.numWheels = numWheels;
+        this.numPassengers = numPassengers;
+    }
+    public String getMake() {
+        return make;
+    }
+    public void setMake(String make) {
+        this.make = make;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getNumWheels() {
+        return numWheels;
+    }
+    public void setNumWheels(int numWheels) {
+        this.numWheels = numWheels;
+    }
+    public int getNumPassengers() {
+        return numPassengers;
+    }
+    public void setNumPassengers(int numPassengers) {
+        this.numPassengers = numPassengers;
+    }
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", numWheels=" + numWheels +
+                ", numPassengers=" + numPassengers +
+                '}';
+    }
+} // End of Vehicle Class
